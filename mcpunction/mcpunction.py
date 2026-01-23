ec302d0effd40cce46eec16796409f5be427a29e
mang-jin <catkinggo113@gmail.com>
2026-01-23 15:58:41 +0900

    improved internal system

--- mcpunction/mcpunction.py
+++ mcpunction/mcpunction.py
@@ -1,6 +1,7 @@
 import inspect
 import os
 import shutil
+from pathlib import Path
 from functools import wraps
 from types import MethodType
 
@@ -40,39 +41,40 @@ def version_to_pack_format(version):
                         break
         return pack_format
 
-cur_file=None
-cur_dtpk=None
-block_count=0
+class FnBuilder:
+        def __init__(self,user_data,output_path):
+                self.user_data=user_data
+                self.output_path=user_data["make_fn_path"](output_path)
+                self.context=[]
+        def raw(self,*args):
+                print(f"output_path: {self.output_path}, context: {self.context}")
+                code="".join(args)
+                print("wrote raw:",code)
+        def close(self):
+                print("FnBuilder Closed")
 
-def raw(*args):
-        global cur_file,cur_dtpk
-        assert cur_file and cur_dtpk
-        code="".join(args)
-        context=" ".join(cur_dtpk._mcpunction_contexts)
-        print("contexts:",cur_dtpk._mcpunction_contexts)
-        print("code:",code)
-        if cur_dtpk._mcpunction_contexts:
-                cur_file.write(f"execute {context} run ")
-        cur_file.write(code+"\n")
+def get_fn_dir(pack_format):
+        return "function" if pack_format>=48 else "functions"
 
-def wrapper(func):
+def wrapper(func,cls):
         if hasattr(func,"_mcpunction_wrapped"):
                 return func
 
         @wraps(func)
         def inner(*args,**kwargs):
-                global cur_file,cur_dtpk
-                is_init=kwargs.pop("_mcpuntion_funcinit",False)
+                user_data=kwargs.pop("_mcpunction_dtpk_data",None)
+                is_init=True if user_data else False
                 is_mac=getattr(func,"_mcpunction_ismac",False)
-                if is_init or is_mac:
-                        if is_init:
-                                last= cur_dtpk._mcpunction_contexts if hasattr(cur_dtpk,"_mcpunction_contexts") else []
-                                cur_dtpk._mcpunction_contexts=[]
-                        result = func(*args,**kwargs)
-                        cur_dtpk._mcpunction_contexts=last
-                        return result
+                if is_mac:
+                        return func(*args,**kwargs)
+                if is_init:
+                        builder=FnBuilder(user_data,f"{cls.__name__}_{func.__name__}")
+                        func(user_data["dtpk"],builder)
+                        builder.close()
                 else:
-                        raw(f"function main:{func.__name__}")
+                        if len(args)>1 or len(kwargs)>0:
+                                raise ValueError("마인크래프트 함수는 인자를 받을 수 없습니다")
+                        raw(f"function main:{cls.__name__}_{func.__name__}")
         inner._mcpunction_wrapped = True
         return inner
 
@@ -83,37 +85,38 @@ class Dtpk:
                 for name, method in inspect.getmembers(cls, inspect.isfunction):
                         if name[0].isupper() or name.startswith("__"):
                                 continue
-                        setattr(cls, name, wrapper(method))
+                        setattr(cls, name, wrapper(method,cls))
 
 class Context:
-        def __init__(self,context):
+        def __init__(self,func_builder,context):
                 self.context=context
+                self.builder=func_builder
         def __enter__(self):
-                global cur_dtpk
-                cur_dtpk._mcpunction_contexts.append(self.context)
+                self.builder.context.append(self.context)
         def __exit__(self,*exec):
-                cur_dtpk._mcpunction_contexts.pop()
+                self.builder.context.pop()
         def __add__(self,other):
                 if not isinstance(other,Context):
                         return
                 new_context = self.context+" "+other.context
                 return Context(new_context)
 
+block_id=0
+
 class Block:
-        # TODO: implement Block
-        def __init__(self,context):
-                self.context=context
+        def __init__(self,fn):
+                global block_id
+
+                block_file_name=f"{block_id}_block.mcfunction"
+                fn.raw(f"function {fn.user_data['namespace']}:{block_id}_block")
+
+                self.fn=FnBuilder(fn.user_data,block_file_name)
+
+                block_id+=1
         def __enter__(self):
-                global cur_dtpk,cur_file
-                self.last=cur_file
-                cur_file=open("",'w')
+                return self.fn
         def __exit__(self,*exec):
-                cur_dtpk.context.pop()
-        def __add__(self,other):
-                if not isinstance(other,Context):
-                        return
-                new_context = self.context+" "+other.context
-                return Context(new_context)
+                self.fn.close()
                 
 def mac(func):
         func._mcpunction_ismac = True
@@ -127,59 +130,76 @@ def ontick(func):
         func._mcpunction_ontick = True
         return func
 
-def make(pkg,output_path,overwrite=False):
-        global cur_file,cur_dtpk,cur_func_dir_path
-        cur_dtpk=pkg
-        namespace=pkg.namespace
-        pack_format=version_to_pack_format(pkg.version)
-        if not hasattr(cur_dtpk,"context") or not isinstance(cur_dtpk.context,list):
-                cur_dtpk.context=[]
-
-        function_dir_name = "function" if pack_format >= 48 else "functions"
-
-        print(f"datapack output path = {output_path}")
-        print(f"datapack namespace = {namespace}")
-        print(f"datapack pack_format = {pack_format}")
-        func_dir_path=f"{output_path}/data/{namespace}/{function_dir_name}"
-        cur_func_dir_path=func_dir_path
-        try:
-                os.makedirs(output_path)
-                os.makedirs(func_dir_path)
-        except FileExistsError:
-                if not overwrite:
-                        yorn = input("이미 폴더가 존재합니다. 덮어쓰시겠습니까? (y/N): ").lower()
-                        if yorn != "y":
-                                print("취소되었습니다.")
-                                return
-                shutil.rmtree(output_path)
-                os.makedirs(func_dir_path)
-        func_tags_dir = f"{output_path}/data/minecraft/tags/{function_dir_name}"
-        os.makedirs(func_tags_dir)
-        load_funcs = []
-        tick_funcs = []
-
-        with open(f"{output_path}/pack.mcmeta",'w') as f:
-                f.write(f'{{"pack":{{"description":"Made with McPunction","pack_format":{pack_format}}}}}')
-
-        for name, method in inspect.getmembers(pkg):
+def make(dtpk: Dtpk,output_path: str,namespace="main"):
+        pack_format=version_to_pack_format(dtpk.version)
+        fn_dir_name="function" if pack_format>=48 else "functions"
+        func_dir_path=f"{output_path}/data/{namespace}/{fn_dir_name}"
+        def make_fn_path(fn_name):
+                return f"{func_dir_path}/{fn_name}.mcfunction"
+        dtpk_data = {
+                "make_fn_path": make_fn_path,
+                "func_dir_path": func_dir_path,
+                "namespace": namespace,
+                "dtpk": dtpk,
+        }
+        for name, method in inspect.getmembers(dtpk):
                 if name[0].isupper() or name.startswith("__"):
                         continue
                 if isinstance(method,MethodType) and not getattr(method,"_mcpunction_ismac",False):
-                        cur_file=open(f"{output_path}/data/{namespace}/{function_dir_name}/{name}.mcfunction",'w')
-                        if getattr(method,"_mcpunction_onload",False):
-                                load_funcs.append(f'"{namespace}:{name}"')
-                        if getattr(method,"_mcpunction_ontick",False):
-                                tick_funcs.append(f'"{namespace}:{name}"')
-                        method(_mcpuntion_funcinit=True)
-                        cur_file.close()
-        with open(f"{func_tags_dir}/load.json",'w') as f:
-                f.write('{"values":[')
-                f.write(",".join(load_funcs))
-                f.write(']}')                        
-        with open(f"{func_tags_dir}/tick.json",'w') as f:
-                f.write('{"values":[')
-                f.write(",".join(tick_funcs))
-                f.write(']}')
-        cur_file=None
-        cur_dtpk=None
-        print("생성 완료")
+                        method(_mcpunction_dtpk_data=dtpk_data)
+        
+
+# class Maker:
+#         def make(self,dtpk,output_path,overwrite=False):
+#                 self.dtpk=dtpk
+#                 self.output_path=output_path
+#                 self.namespace=dtpk.namespace
+#                 self.pack_format=version_to_pack_format(self.dtpk.version)                
+#                 if not hasattr(cur_dtpk,"context") or not isinstance(cur_dtpk.context,list):
+#                         self.dtpk.context=[]
+#                 self.function_dir_name = "function" if self.pack_format >= 48 else "functions"
+#                 print(f"datapack output path = {self.output_path}")
+#                 print(f"datapack namespace = {self.namespace}")
+#                 print(f"datapack pack_format = {self.pack_format}")
+#                 self.func_dir_path=f"{self.output_path}/data/{self.namespace}/{self.function_dir_name}"
+#                 try:
+#                         os.makedirs(self.output_path)
+#                         os.makedirs(self.func_dir_path)
+#                 except FileExistsError:
+#                         if not overwrite:
+#                                 yorn = input("이미 폴더가 존재합니다. 덮어쓰시겠습니까? (y/N): ").lower()
+#                                 if yorn != "y":
+#                                         print("취소되었습니다.")
+#                                         return
+#                         shutil.rmtree(self.output_path)
+#                         os.makedirs(self.func_dir_path)
+#                 self.func_tags_dir = f"{self.output_path}/data/minecraft/tags/{self.function_dir_name}"
+#                 os.makedirs(self.func_tags_dir)
+#                 self.load_funcs = []
+#                 self.tick_funcs = []
+
+#                 with open(f"{output_path}/pack.mcmeta",'w') as f:
+#                         f.write(f'{{"pack":{{"description":"Made with McPunction","pack_format":{self.pack_format}}}}}')
+#                 for name, method in inspect.getmembers(self.dtpk):
+#                         if name[0].isupper() or name.startswith("__"):
+#                                 continue
+#                         if isinstance(method,MethodType) and not getattr(method,"_mcpunction_ismac",False):
+#                                 with open(f"{self.output_path}/data/{self.namespace}/{self.function_dir_name}/{name}.mcfunction",'w') as cur_file:
+#                                         self.cur_file=cur_file
+#                                         if getattr(method,"_mcpunction_onload",False):
+#                                                 load_funcs.append(f'"{self.namespace}:{name}"')
+#                                         if getattr(method,"_mcpunction_ontick",False):
+#                                                 tick_funcs.append(f'"{self.namespace}:{name}"')
+#                                         method(_mcpuntion_funcinit=True)
+#                 with open(f"{self.func_tags_dir}/load.json",'w') as f:
+#                         f.write('{"values":[')
+#                         f.write(",".join(self.load_funcs))
+#                         f.write(']}')                        
+#                 with open(f"{self.func_tags_dir}/tick.json",'w') as f:
+#                         f.write('{"values":[')
+#                         f.write(",".join(self.tick_funcs))
+#                         f.write(']}')
+#                 self.cur_file=None
+#                 self.dtpk=None
+#                 print("생성 완료")
+
--- tests/test_mcpunction.py
+++ tests/test_mcpunction.py
@@ -4,21 +4,12 @@ class pkg(pn.Dtpk):
         def __init__(self):
                 self.version="1.21.11"
                 self.namespace="main"
-        @pn.mac
-        def player_shooting_gun(self):
-                return pn.Context("as @a[tag=used_gun]")
-        def shoot_gun(self):
-                pn.raw("say phew!")
-        @pn.mac
-        def has_gun_in_offhand(self):
-                return pn.Context("if items entity @s weapon.offhand gun")
-        @pn.ontick
-        def game_loop(self):
-                with self.player_shooting_gun():
-                        self.shoot_gun()
-                        pn.raw("tag @s remove used_gun")
-                with pn.Context("as @a")+self.has_gun_in_offhand():
-                        pn.raw("tag @s add used_gun")
-                
+                return
+        def main(self,fn):
+                with pn.Context(fn,"as @a"):
+                        with pn.Block(fn) as fn:
+                                fn.raw("hello")
+                fn.raw("say hi")
+                return
 p=pkg()
-pn.make(p,"../test_dtpk",overwrite=True)
+pn.make(p,"../test_dtpk")
